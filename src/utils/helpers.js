2138c75f9187293723dad8a399a1978711639c3c
Manik Kumar <mnk_400@icloud.com>
2026-09-03 22:19:16 -0700

    chore: remove unused frontend modules

--- src/utils/helpers.js
+++
@@ -1,9 +0,0 @@
-// Array utilities
-export function shuffleArray(array) {
-  const newArray = [...array];
-  for (let i = newArray.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
-  }
-  return newArray;
-}
